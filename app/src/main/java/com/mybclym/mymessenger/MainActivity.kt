356004dfc7443c14c8c39d74e8ba6c11664df524
Eugen <Alexandrov>
2020-07-29 15:46:07 +0300

    add NavigationDrawer

--- app/src/main/java/com/mybclym/mymessenger/MainActivity.kt
+++ app/src/main/java/com/mybclym/mymessenger/MainActivity.kt
@@ -2,10 +2,65 @@ package com.mybclym.mymessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mybclym.mymessenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawer: Drawer
+    private lateinit var header: AccountHeader
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+
+    }
+
+    private fun initFields() {
+        toolbar = binding.mainToolBar
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(toolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        drawer = DrawerBuilder().withActivity(this)
+            .withToolbar(toolbar)
+            .withActionBarDrawerToggleAnimated(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(header)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(true)
+            ).build()
+    }
+
+    private fun createHeader() {
+        header = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Eugen").withEmail("+79608665666")
+            ).build()
     }
 }
